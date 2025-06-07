d656516dad3a76baea8a61cb9f248670a7317ede
Natalia <ngawronska@gmail.com>
2025-06-07 16:58:53 +0200

    WHITE TEXT ON DARK BACKGROUND ON A BUTTON
    Feature that changes the class of text into
    bright, when the button is active and the
    background turns too dark for a default text.

--- src/pastel-ui-library/components/Button/Button.tsx
+++ src/pastel-ui-library/components/Button/Button.tsx
@@ -8,12 +8,11 @@ interface ButtonProps {
 
 interface ButtonColorScheme {
   text: string;
-  deleteBg: string;
-  borderAct: string;
   textOnDark?: string;
   textDis: string;
   acceptBg: string;
   readMoreBg: string;
+  deleteBg: string;
   disabledBg: string;
   hoverBg: string;
   activeBg: string;
@@ -21,6 +20,7 @@ interface ButtonColorScheme {
   borderDel: string;
   borderDis: string;
   outlineFoc: string;
+  borderAct: string;
 }
 
 const baseScheme = {
@@ -35,30 +35,31 @@ const colorSchemes: Record<
 > = {
   danger: {
     ...baseScheme,
-    textOnDark: "text-button-text-on-dark",
+    textOnDark: "active:text-button-text-on-dark",
     textDis: "text-button-danger-disabled-text",
     acceptBg: "bg-button-danger-primary",
     readMoreBg: "bg-button-danger-secondary",
     disabledBg: "bg-button-danger-disabled",
-    hoverBg: "bg-button-danger-hover",
-    activeBg: "bg-button-danger-active",
+    hoverBg: "hover:bg-button-danger-hover",
+    activeBg: "active:bg-button-danger-active",
     border: "border-button-danger-stroke",
     borderDel: "border-button-danger-delete-stroke",
     borderDis: "border-button-danger-disabled-stroke",
-    outlineFoc: "outline-button-danger-focus-outline",
+    outlineFoc: "focus:outline-button-danger-focus-outline",
   },
   success: {
     ...baseScheme,
+    textOnDark: "active:text-button-text-on-dark",
     textDis: "text-button-success-disabled-text",
     acceptBg: "bg-button-success-primary",
     readMoreBg: "bg-button-success-secondary",
     disabledBg: "bg-button-success-disabled",
-    hoverBg: "bg-button-success-hover",
-    activeBg: "bg-button-success-active",
+    hoverBg: "hover:bg-button-success-hover",
+    activeBg: "active:bg-button-success-active",
     border: "border-button-success-stroke",
     borderDel: "border-button-success-delete-stroke",
     borderDis: "border-button-success-disabled-stroke",
-    outlineFoc: "outline-button-success-focus-outline",
+    outlineFoc: "focus:outline-button-success-focus-outline",
   },
   warning: {
     ...baseScheme,
@@ -66,40 +67,40 @@ const colorSchemes: Record<
     acceptBg: "bg-button-warning-primary",
     readMoreBg: "bg-button-warning-secondary",
     disabledBg: "bg-button-warning-disabled",
-    hoverBg: "bg-button-warning-hover",
-    activeBg: "bg-button-warning-active",
+    hoverBg: "hover:bg-button-warning-hover",
+    activeBg: "active:bg-button-warning-active",
     border: "border-button-warning-stroke",
     borderDel: "border-button-warning-delete-stroke",
     borderDis: "border-button-warning-disabled-stroke",
-    outlineFoc: "outline-button-warning-focus-outline",
+    outlineFoc: "focus:outline-button-warning-focus-outline",
   },
   info: {
     ...baseScheme,
-    textOnDark: "text-button-text-on-dark",
+    textOnDark: "active:text-button-text-on-dark",
     textDis: "text-button-info-disabled-text",
     acceptBg: "bg-button-info-primary",
     readMoreBg: "bg-button-info-secondary",
     disabledBg: "bg-button-info-disabled",
-    hoverBg: "bg-button-info-hover",
-    activeBg: "bg-button-info-active",
+    hoverBg: "hover:bg-button-info-hover",
+    activeBg: "active:bg-button-info-active",
     border: "border-button-info-stroke",
     borderDel: "border-button-info-delete-stroke",
     borderDis: "border-button-info-disabled-stroke",
-    outlineFoc: "outline-button-info-focus-outline",
+    outlineFoc: "focus:outline-button-info-focus-outline",
   },
   muted: {
     ...baseScheme,
-    textOnDark: "text-button-text-on-dark",
+    textOnDark: "active:text-button-text-on-dark",
     textDis: "text-button-muted-disabled-text",
     acceptBg: "bg-button-muted-primary",
     readMoreBg: "bg-button-muted-secondary",
     disabledBg: "bg-button-muted-disabled",
-    hoverBg: "bg-button-muted-hover",
-    activeBg: "bg-button-muted-active",
+    hoverBg: "hover:bg-button-muted-hover",
+    activeBg: "active:bg-button-muted-active",
     border: "border-button-muted-stroke",
     borderDel: "border-button-muted-delete-stroke",
     borderDis: "border-button-muted-disabled-stroke",
-    outlineFoc: "outline-button-muted-focus-outline",
+    outlineFoc: "focus:outline-button-muted-focus-outline",
   },
 };
 
@@ -111,18 +112,15 @@ const Button: React.FC<ButtonProps> = ({
   const scheme = colorSchemes[colorScheme];
 
   if (!scheme) {
-    // fallback if colorScheme is invalid
     return <button>{children}</button>;
   }
 
   const variant = children.toLowerCase();
-
   const baseClasses =
     "font-inter font-semibold border px-6 py-2 transition gap-5 focus:outline-solid focus:outline-2 focus:outline-offset-2";
 
   const isDisabled = variant === "disabled";
 
-  // Disabled button styles
   if (isDisabled) {
     return (
       <button
@@ -141,10 +139,14 @@ const Button: React.FC<ButtonProps> = ({
     );
   }
 
-  // Non-disabled buttons styling based on variant
   let bgClass = "";
   let borderClass = "";
   let textClass = scheme.text;
+  let activeTextClass = "";
+
+  const variantUsesDarkActiveBg =
+    ["accept", "read more", "delete"].includes(variant) &&
+    ["success", "muted", "info", "danger"].includes(colorScheme);
 
   switch (variant) {
     case "accept":
@@ -164,41 +166,21 @@ const Button: React.FC<ButtonProps> = ({
       borderClass = scheme.border;
   }
 
-  const hoverClasses = {
-    danger: "hover:bg-button-danger-hover",
-    success: "hover:bg-button-success-hover",
-    warning: "hover:bg-button-warning-hover",
-    info: "hover:bg-button-info-hover",
-    muted: "hover:bg-button-muted-hover",
-  };
-
-  const activeClasses = {
-    danger: "active:bg-button-danger-active",
-    success: "active:bg-button-success-active",
-    warning: "active:bg-button-warning-active",
-    info: "active:bg-button-info-active",
-    muted: "active:bg-button-muted-active",
-  };
-
-  const focusClasses = {
-    danger: "focus:outline-button-danger-focus-outline",
-    success: "focus:outline-button-success-focus-outline",
-    warning: "focus:outline-button-warning-focus-outline",
-    info: "focus:outline-button-info-focus-outline",
-    muted: "focus:outline-button-muted-focus-outline",
-  };
+  if (variantUsesDarkActiveBg && scheme.textOnDark) {
+    activeTextClass = scheme.textOnDark;
+  }
 
   return (
     <button
       className={classNames(
         baseClasses,
         textClass,
+        activeTextClass,
         bgClass,
         borderClass,
         "cursor-pointer",
-        hoverClasses[colorScheme],
-        activeClasses[colorScheme],
-        focusClasses[colorScheme],
+        scheme.hoverBg,
+        scheme.activeBg,
         scheme.outlineFoc,
         className
       )}
