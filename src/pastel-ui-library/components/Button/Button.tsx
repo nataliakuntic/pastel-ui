52f9f8b745ede2c55afb3a389117ae9ea8ed029a
Natalia <ngawronska@gmail.com>
2025-06-22 22:08:56 +0200

    NAME FIXING, DISABLED BUTTON BG FIXED
    Better prop naming provided, further clean-up of
    color scheme names, disabled button color not
    displaying bug fixed

--- src/pastel-ui-library/components/Button/Button.tsx
+++ src/pastel-ui-library/components/Button/Button.tsx
@@ -28,15 +28,16 @@ const Button: React.FC<ButtonProps> = ({
     bgActive,
     border,
     borderDisabled,
-    outlineFoc,
+    bgDisabled,
+    outlineFocus,
   } = getClassesForVariant(theme, variant);
 
   const buttonClass = classNames(
     "font-inter font-semibold border px-6 py-2 transition gap-5 focus:outline-solid focus:outline-2 focus:outline-offset-2",
     disabled ? textDisabled : text,
-    bg,
+    disabled ? bgDisabled : bg,
     disabled ? borderDisabled : border,
-    outlineFoc,
+    outlineFocus,
     rounded && "rounded-full",
     disabled ? "cursor-not-allowed select-none" : "cursor-pointer",
     !disabled && bgHover,
