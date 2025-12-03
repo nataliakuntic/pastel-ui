b126b7db4201adb1ef06aaf1833f15f82b9d872d
Natalia <ngawronska@gmail.com>
2025-12-03 11:10:57 +0100

    Add custom checkbox
    Replaced browser styled checkbox type of input and built a default new
    one from scratch as a placeholder before the Pastel UI design themes
    get applied.

--- src/pastel-ui-library/components/Checkbox/Checkbox.tsx
+++ src/pastel-ui-library/components/Checkbox/Checkbox.tsx
@@ -16,16 +16,39 @@ const Checkbox: React.FC<CheckboxProps> = ({
   label,
 }) => {
   return (
-    <div>
+    <div className="flex gap-2">
       <input
         type="checkbox"
+        className="
+  relative peer shrink-0
+  appearance-none w-4 h-4 border-2 border-blue-500 bg-white
+  mt-1
+  checked:bg-blue-800 checked:border-0
+  focus:outline-none focus:ring-offset-0 focus:ring-2 focus:ring-blue-100
+  disabled:border-steel-400 disabled:bg-steel-400
+"
         checked={isChecked}
         onChange={() => onChange(!isChecked)}
         disabled={isDisabled}
         required={isRequired}
       />
       <label>{label}</label>
-      <Check />
+      <svg
+        className="
+    absolute 
+    w-4 h-4 mt-1
+    hidden peer-checked:block
+    pointer-events-none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#fff"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
     </div>
   );
 };
