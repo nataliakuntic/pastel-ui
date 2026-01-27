01390c60f8558446f976ff1d50b589ee40bcea24
Natalia <ngawronska@gmail.com>
2026-01-27 12:54:28 +0100

    Puts Checkbox input inside the label
    Went for a best way in React to present input element semantically.
    Removed the case of useId and id as a prop. Instead, put input
    and svg into the label.
    
    Classes were adjusted accordingly to the new setup.

--- src/pastel-ui-library/components/Checkbox/Checkbox.tsx
+++ src/pastel-ui-library/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,5 @@
 import { ThemeName } from "../../types";
 import { getCheckboxThemeStyle } from "./checkboxThemes";
-import { useId } from "react";
 import classNames from "classnames";
 
 interface CheckboxProps {
@@ -22,70 +21,67 @@ const Checkbox: React.FC<CheckboxProps> = ({
   isRequired,
   label,
 }) => {
-  const id = useId();
-
   const theme = getCheckboxThemeStyle(colorScheme);
   const variantStyles = theme.variants[variant];
 
-  const wrapperClass = classNames(
+  const labelClass = classNames(
     "flex",
+    "items-center",
     "gap-2",
-    "flex-grow-0",
+    theme.labelText,
+    isDisabled ? "cursor-not-allowed select-none" : "cursor-pointer",
+  );
+
+  const wrapperClass = classNames(
+    "relative",
+    "flex",
+    "items-center",
+    "justify-center",
+    "gap-2",
+    "items-start",
+    "w-4",
+    "h-4",
+    "shrink-0",
+    "transition-colors",
+    "duration-200",
     isDisabled ? "cursor-not-allowed select-none" : "cursor-pointer",
   );
 
   const checkboxClass = classNames(
-    "relative",
     "peer",
-    "shrink-0",
+    "absolute",
+    "inset-0",
     "appearance-none",
-    "w-4",
-    "h-4",
-    "border-2",
-    "mt-1",
+    "w-full",
+    "h-full",
     "focus:outline-none",
     "focus:ring-offset-0",
     "focus:ring-2",
-    "transition-colors",
-    "duration-200",
-    "ease-out",
-    !isDisabled && isChecked ? variantStyles.bgChecked : "",
-    !isDisabled && !isChecked ? variantStyles.bgUnchecked : "",
+    "transition",
     !isDisabled && variantStyles.ringFocus,
     isDisabled ? theme.borderDisabled : variantStyles.border,
     isDisabled ? theme.bgDisabled : "",
-  );
-
-  const labelClass = classNames(
-    theme.labelText,
-    isDisabled ? "cursor-not-allowed select-none" : "cursor-pointer",
+    !isDisabled && isChecked ? variantStyles.bgChecked : "",
+    !isDisabled && !isChecked ? variantStyles.bgUnchecked : "",
   );
 
   const iconClass = classNames(
     "absolute",
     "w-4",
     "h-4",
-    "mt-1",
-    "hidden",
-    "peer-checked:block",
-    "pointer-events-none",
-    "scale-0",
-    "opacity-0",
-    "peer-checked:scale-100",
-    "peer-checked:opacity-100",
-    "peer-checked:delay-[20ms]",
     "transition-all",
     "duration-300",
-    "ease-out",
-    "motion-reduce:transition-none",
+    "opacity-0",
+    "scale-75",
+    "peer-checked:opacity-100",
+    "peer-checked:scale-100",
     isDisabled ? theme.iconCheckedDisabled : variantStyles.iconChecked,
   );
 
   return (
-    <div className="flex">
-      <div className={wrapperClass}>
+    <label className={labelClass}>
+      <span className={wrapperClass}>
         <input
-          id={id}
           type="checkbox"
           className={checkboxClass}
           checked={isChecked}
@@ -93,9 +89,6 @@ const Checkbox: React.FC<CheckboxProps> = ({
           disabled={isDisabled}
           required={isRequired}
         />
-        <label htmlFor={id} className={labelClass}>
-          {label}
-        </label>
         <svg
           className={iconClass}
           aria-hidden="true"
@@ -109,8 +102,9 @@ const Checkbox: React.FC<CheckboxProps> = ({
         >
           <polyline points="20 6 9 17 4 12" />
         </svg>
-      </div>
-    </div>
+      </span>
+      {label && <span>{label}</span>}
+    </label>
   );
 };
 
