81e04db5e9c6af96291ae03bffcee53cac49dcf7
Natalia <ngawronska@gmail.com>
2026-08-05 12:31:31 +0200

    Extract pastel-ui-website into separate repo
    Extracted pastel-ui-website into its own repo, in preparation
    of releasing version 0.0.1 of pastel-ui-library.

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+
+import "./index.css";
+import Accordion from "./components/Accordion/Accordion";
+import Button from "./components/Button/Button";
+import LargeCard from "./components/Card/LargeCard";
+import MediumCard from "./components/Card/MediumCard";
+import SmallCard from "./components/Card/SmallCard";
+import Checkbox from "./components/Checkbox/Checkbox";
+import TimePicker from "./components/DateTimePicker/TimePicker";
+import DatePicker from "./components/DateTimePicker/DatePicker";
+import Radiogroup from "./components/Radiobutton/Radiogroup";
+import Select from "./components/Select/Select";
+import TextInput from "./components/TextInput/TextInput";
+import TextArea from "./components/TextArea/TextArea";
+import { ThemeName } from "./types";
+import { AccordionThemeName } from "./components/Accordion/accordionThemes";
+
+export type { ThemeName, AccordionThemeName };
+
+export {
+    Accordion,
+    Button,
+    LargeCard,
+    MediumCard,
+    SmallCard,
+    Checkbox,
+    TimePicker,
+    DatePicker,
+    Radiogroup,
+    Select,
+    TextInput,
+    TextArea
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
 import svgr from "vite-plugin-svgr";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'PastelUI',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'vue'],
+      output: {
+        globals: {
+          react: 'React',
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    svgr(),
+    dts({
+      bundleTypes: true,
+      tsconfigPath: './tsconfig.app.json'
+    }),
+    react()],
+});
