b3bc2ce902605805cdd6bc3568a177ec0358140d
Natalia <ngawronska@gmail.com>
2026-08-16 19:38:38 +0200

    Export the  AccordionItem, RadioOption and SelectOption types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import "./index.css";
-import Accordion from "./components/Accordion/Accordion";
+import Accordion, { AccordionItem } from "./components/Accordion/Accordion";
 import Button from "./components/Button/Button";
 import LargeCard from "./components/Card/LargeCard";
 import MediumCard from "./components/Card/MediumCard";
@@ -8,14 +8,14 @@ import SmallCard from "./components/Card/SmallCard";
 import Checkbox from "./components/Checkbox/Checkbox";
 import TimePicker from "./components/DateTimePicker/TimePicker";
 import DatePicker from "./components/DateTimePicker/DatePicker";
-import Radiogroup from "./components/Radiobutton/Radiogroup";
-import Select from "./components/Select/Select";
+import Radiogroup, { RadioOption } from "./components/Radiobutton/Radiogroup";
+import Select, { SelectOption } from "./components/Select/Select";
 import TextInput from "./components/TextInput/TextInput";
 import TextArea from "./components/TextArea/TextArea";
 import { ThemeName } from "./types";
 import { AccordionThemeName } from "./components/Accordion/accordionThemes";
 
-export type { ThemeName, AccordionThemeName };
+export type { ThemeName, AccordionThemeName, AccordionItem, RadioOption, SelectOption };
 
 export {
     Accordion,
